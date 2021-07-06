6bc04fae376bcac58ca0702e2a4a0972cb21104d
ABDBXB <66337063+ABDBXB@users.noreply.github.com>
2021-07-06 11:23:24 +0300

    Upload Loading Page

---
+++ Loading Page/WindowsFormsApp1/Form33.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form33
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form33));
+            this.CProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CProgressBar1
+            // 
+            this.CProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.CProgressBar1.AnimationSpeed = 500;
+            this.CProgressBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(40)))), ((int)(((byte)(60)))));
+            this.CProgressBar1.Font = new System.Drawing.Font("Cambria Math", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(213)))), ((int)(((byte)(219)))));
+            this.CProgressBar1.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(40)))), ((int)(((byte)(60)))));
+            this.CProgressBar1.InnerMargin = 2;
+            this.CProgressBar1.InnerWidth = -1;
+            this.CProgressBar1.Location = new System.Drawing.Point(51, 57);
+            this.CProgressBar1.MarqueeAnimationSpeed = 2000;
+            this.CProgressBar1.Maximum = 140;
+            this.CProgressBar1.Name = "CProgressBar1";
+            this.CProgressBar1.OuterColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(28)))), ((int)(((byte)(43)))));
+            this.CProgressBar1.OuterMargin = -25;
+            this.CProgressBar1.OuterWidth = 26;
+            this.CProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(213)))), ((int)(((byte)(219)))));
+            this.CProgressBar1.ProgressWidth = 6;
+            this.CProgressBar1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.CProgressBar1.Size = new System.Drawing.Size(173, 171);
+            this.CProgressBar1.StartAngle = 270;
+            this.CProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.CProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.CProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.CProgressBar1.SubscriptText = "";
+            this.CProgressBar1.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(213)))), ((int)(((byte)(219)))));
+            this.CProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.CProgressBar1.SuperscriptText = "";
+            this.CProgressBar1.TabIndex = 0;
+            this.CProgressBar1.Text = "100%";
+            this.CProgressBar1.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.CProgressBar1.Value = 68;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(213)))), ((int)(((byte)(220)))));
+            this.label1.Location = new System.Drawing.Point(78, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "İŞLEM";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(213)))), ((int)(((byte)(220)))));
+            this.label2.Location = new System.Drawing.Point(53, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Devam Ediyor..";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(92, 98);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(40)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(265, 296);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CProgressBar1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.85D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CircularProgressBar.CircularProgressBar CProgressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Loading Page/WindowsFormsApp1/Form33.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form33 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+(
+   int nLeftRect,     // x-coordinate of upper-left corner
+   int nTopRect,      // y-coordinate of upper-left corner
+   int nRightRect,    // x-coordinate of lower-right corner
+   int nBottomRect,   // y-coordinate of lower-right corner
+   int nWidthEllipse, // height of ellipse
+   int nHeightEllipse // width of ellipse
+);
+
+        public Form33()
+        {
+            
+            InitializeComponent();
+            this.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+            CProgressBar1.Value = 0;
+
+        }
+
+        //public string[] dots = {"..","...","..." };
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CProgressBar1.Value += 5;
+            
+            if (CProgressBar1.Value < 100)
+            {
+
+                CProgressBar1.Text = CProgressBar1.Value.ToString() + "%";
+            }
+            if(CProgressBar1.Value == 100)
+            {
+                CProgressBar1.Text = "";
+                pictureBox1.Visible = true;
+                label2.Text = "TAMAMLANDI";
+            }
+            if (CProgressBar1.Value == 140)
+            {
+                this.Close();
+            }
+            //for (int i=0; i < dots.Length - 1; i++)
+            //{
+            //    label2.Text = label2.Text + dots[i];
+
+            //    if (i == 2)
+            //    {
+            //        i = 0;
+            //    }
+            //}
+
+
+
+
+
+        }
+    }
+}
