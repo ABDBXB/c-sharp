b393869121a8022662762d7c738266936eaa74a4
ABDBXB <66337063+ABDBXB@users.noreply.github.com>
2021-07-06 11:19:36 +0300

    Upload Save As PDF Program

---
+++ save as pdf/save as pdf/Form1.cs
@@ -0,0 +1,45 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace save_as_pdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tab = "              ";
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("yemek adı"+tab);
+            stringBuilder.Append("Fiyat \n");
+            stringBuilder.Append("----------------\n");
+            stringBuilder.Append("Garsonadı"+tab);
+            stringBuilder.Append("Adet \n");
+            stringBuilder.Append("====================\n");
+
+            Document doc = new Document(PageSize.A4.Rotate());
+            PdfWriter.GetInstance(doc, new FileStream("C:/Users/abdal/Desktop/FaturaNo.pdf", FileMode.Create));
+            doc.Open();
+            doc.Add(new Paragraph(stringBuilder.ToString()));
+            doc.Close();
+            MessageBox.Show("Fatura Basıldı");
+
+
+
+        }
+    }
+}
---
+++ save as pdf/save as pdf/form.cs
@@ -0,0 +1,72 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace save_as_pdf
+{
+    public partial class form : Form
+    {
+        public form()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Document doc = new Document(PageSize.A4.Rotate());
+            PdfWriter.GetInstance(doc, new FileStream("FaturaNo.pdf", FileMode.Create));
+
+
+            //                                      columns
+            var headerTable = new PdfPTable(new[] { .1f, .02f,.5f })
+            {
+                HorizontalAlignment = Left,
+
+                WidthPercentage = 50,
+
+                DefaultCell = { MinimumHeight = 22f }
+
+            };
+            var cell = new PdfPCell(new Phrase("Fatura No"));
+            cell.Border = 0;
+            headerTable.AddCell(cell);
+
+            cell = new PdfPCell(new Phrase(":"));
+            cell.Border = 0;
+            headerTable.AddCell(cell);
+
+
+
+            cell = new PdfPCell(new Phrase("22"));
+            cell.Border = 0;
+            headerTable.AddCell(cell);
+
+            cell = new PdfPCell(new Phrase("Date"));
+            cell.Border = 0;
+            headerTable.AddCell(cell);
+
+            cell = new PdfPCell(new Phrase(":"));
+            cell.Border = 0;
+            headerTable.AddCell(cell);
+
+            cell = new PdfPCell(new Phrase(DateTime.Now.ToString()));
+            cell.Border = 0;
+            headerTable.AddCell(cell);
+
+
+
+            doc.Open();
+            doc.Add(headerTable);
+            doc.Close();
+        }
+    }
+}
