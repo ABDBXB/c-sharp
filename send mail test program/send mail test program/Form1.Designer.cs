65954db1a53439461c726157c7ff73a7fcdfb129
ABDBXB <66337063+ABDBXB@users.noreply.github.com>
2021-07-06 11:14:18 +0300

    Upload Send Mail Program

---
+++ send mail test program/send mail test program/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace send_mail_test_program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxsubject = new System.Windows.Forms.TextBox();
+            this.textBoxmessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnsend = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxto
+            // 
+            this.textBoxto.Location = new System.Drawing.Point(65, 22);
+            this.textBoxto.Name = "textBoxto";
+            this.textBoxto.Size = new System.Drawing.Size(163, 20);
+            this.textBoxto.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TO";
+            // 
+            // textBoxsubject
+            // 
+            this.textBoxsubject.Location = new System.Drawing.Point(65, 59);
+            this.textBoxsubject.Name = "textBoxsubject";
+            this.textBoxsubject.Size = new System.Drawing.Size(163, 20);
+            this.textBoxsubject.TabIndex = 2;
+            // 
+            // textBoxmessage
+            // 
+            this.textBoxmessage.Location = new System.Drawing.Point(65, 109);
+            this.textBoxmessage.Multiline = true;
+            this.textBoxmessage.Name = "textBoxmessage";
+            this.textBoxmessage.Size = new System.Drawing.Size(205, 92);
+            this.textBoxmessage.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Subject";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Message";
+            // 
+            // btnsend
+            // 
+            this.btnsend.Location = new System.Drawing.Point(195, 207);
+            this.btnsend.Name = "btnsend";
+            this.btnsend.Size = new System.Drawing.Size(75, 23);
+            this.btnsend.TabIndex = 6;
+            this.btnsend.Text = "Send";
+            this.btnsend.UseVisualStyleBackColor = true;
+            this.btnsend.Click += new System.EventHandler(this.btnsend_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(84, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 18);
+            this.label4.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(722, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(5, 292);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "SHA256:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 361);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnsend);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxmessage);
+            this.Controls.Add(this.textBoxsubject);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxsubject;
+        private System.Windows.Forms.TextBox textBoxmessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnsend;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ send mail test program/send mail test program/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Security.Cryptography;
+
+namespace send_mail_test_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+        
+        
+        string[] password = { "A", "1", "F", "B", "W", "Y", "9", "5", "I", "Q", "H", "C", "J", "K", "D", "M", "l", "L", "E", "O", "Z" };
+        string pass = null;
+        string sifrelenmis;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+        }
+
+        private void btnsend_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            try {
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("YourGmail@gmail.com", "YourGmailPassord");
+                MailMessage msg = new MailMessage();
+                msg.To.Add(textBoxto.Text);
+                msg.From = new MailAddress("YourGmail@gmail.com");
+                msg.Subject = textBoxsubject.Text;
+                msg.Body = textBoxmessage.Text;
+                client.Send(msg);
+                MessageBox.Show("Done : ) ","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+            }
+            catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int number = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                number = rand.Next(password.Length);
+                pass = pass + password[number].ToString();
+            }
+            textBoxsubject.Text = "Konya Restoran Şifre Yenileme.";
+            textBoxmessage.Text = "sayin Kullaıcı Yeni şifreniz: " + pass;
+            
+            SHA256 sHA256 = new SHA256CryptoServiceProvider();
+            byte[] bytes = sHA256.ComputeHash(Encoding.UTF8.GetBytes(pass));
+            StringBuilder builder = new StringBuilder();
+            foreach (var item in bytes)
+            {
+                builder.Append(item.ToString("x2"));
+            }
+
+            label4.Text = builder.ToString();
+            pass = null;
+        }
+    }
+}
